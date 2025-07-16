fc285f40f935c4cf733fcf8a84e4e9adccf7c969
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-07-16 19:10:21 +0100

    implement get genre route

--- app.js
+++ app.js
@@ -20,6 +20,17 @@ app.get("/api/genres", (req, res) => {
   res.send(genres);
 });
 
+// Get genre
+app.get("/api/genres/:id", (req, res) => {
+  // Find genre in array, if does not exist, return 404
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found.");
+
+  // Return the genre
+  res.send(genre);
+});
+
 
 // Express "endpoint" event emitter
 const port = process.env.PORT || 3000;
