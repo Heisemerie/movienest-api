7cb85b1e39d35d6de02bea90f1e2fdf899dfa3da
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-07-16 19:09:34 +0100

    implement get all route

--- app.js
+++ app.js
@@ -6,3 +6,23 @@ const app = express();
 // Middleware
 app.use(express.json());
 
+const genres = [
+  { id: 1, name: "horror" },
+  { id: 2, name: "comedy" },
+  { id: 3, name: "romance" },
+  { id: 4, name: "action" },
+  { id: 5, name: "thriller" },
+];
+
+// Routes
+// Get all genres
+app.get("/api/genres", (req, res) => {
+  res.send(genres);
+});
+
+
+// Express "endpoint" event emitter
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+}); 
