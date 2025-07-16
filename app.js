66e76962d52a88cd80d74a28201fd41d82f682dc
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-07-16 19:12:21 +0100

    implement put genre route

--- app.js
+++ app.js
@@ -47,6 +47,24 @@ app.post("/api/genres", (req, res) => {
   res.send(genre);
 });
 
+app.put("/api/genres/:id", (req, res) => {
+  // Look up the genre
+  // If the genre does not exist, return 404 - Not found
+  const genre = genres.find((g) => g.id === parseInt(req.params.id));
+  if (!genre)
+    return res.status(404).send("The genre with the given ID was not found");
+
+  // Validate
+  // If invalid, return 400 - bad request
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Update genre
+  // Return the updated genre
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
 // Express "endpoint" event emitter
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
