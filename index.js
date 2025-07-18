323c98f5c5d10fb407ea35a320503695ade7bf13
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-07-18 09:40:20 +0100

    Refactor: rename "genre.js" routes to "genres.js"

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const genres = require("./routes/genre");
+const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const mongoose = require("mongoose");
 
