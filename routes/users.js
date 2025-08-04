9e30a3bcfd7dfd880fcb65f35b2eb8009160481d
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-04 20:00:19 +0100

    implement password hashing with bcrypt

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,14 @@
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 const { schema, User } = require("../models/user");
+
+// Get All
+router.get("/", async (req, res) => {
+  const users = await User.find().sort({ name: 1 });
+  res.send(users);
+});
 
 // Post user in DB
 router.post("/", async (req, res) => {
@@ -12,7 +19,9 @@ router.post("/", async (req, res) => {
   const prevUser = await User.findOne({ email: req.body.email });
   if (prevUser) return res.status(400).send("This user already exists.");
 
-  const user = new User(_.pick(req.body, ["name", "email", "password"]));
+  let user = new User(_.pick(req.body, ["name", "email", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt); // hash the password
   const result = await user.save();
 
   res.send(_.pick(result, ["_id", "name", "email"])); // do not return the password
