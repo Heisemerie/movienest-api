44efdfd74f13c874f25c2eebea41bfc598b61172
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-04 22:11:36 +0100

    implement user authentication

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
 const mongoose = require("mongoose");
 
 const app = express();
@@ -25,6 +26,7 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 // Express "endpoint" event emitter
 const port = process.env.PORT || 3000; // get port from environment variable or use port 3000
---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const Joi = require("joi");
+const bcrypt = require("bcrypt");
+const _ = require("lodash");
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/user");
+
+// Joi auth schema
+const schema = Joi.object({
+  email: Joi.string().email().min(5).max(255).required(),
+  password: Joi.string().min(5).max(255).required(),
+});
+
+// Post auth in DB
+router.post("/", async (req, res) => {
+  const { error } = schema.validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Validate email (check user exists)
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send("Invalid email or password");
+
+  // Validate password (compare passwords)
+  const validPassword = bcrypt.compare(req.body.password, user.password); // compare plaintext and hashed password
+  if (!validPassword) return res.status(400).send("Invalid email or password");
+
+  res.send(true); 
+});
+
+module.exports = router;
