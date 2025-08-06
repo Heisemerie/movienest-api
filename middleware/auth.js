00509defbd5fdf2f3d72ca93b7b4e9c60308d5ce
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-06 21:06:47 +0100

    implement authorization middleware

---
+++ middleware/auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
+  if (!token) return res.status(401).send("Access denied. No token provided.");
+
+  try {
+    const decoded = jwt.verify(token, config.length("jwtPrivateKey")); // decode the token and extract the payload
+    req.user = decoded; // set the payload in the response
+    next(); // call the next middleware function in the pipeline
+  } catch (error) {
+    res.status(400).send("Invalid token.");
+  }
+}
+
+module.exports = auth;
