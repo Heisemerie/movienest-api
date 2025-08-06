33f5a087645e327a50c28fbcac47fe8319386cd0
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-07 00:21:05 +0100

    fix: change decoded to payload

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,8 +6,8 @@ function auth(req, res, next) {
   if (!token) return res.status(401).send("Access denied. No token provided.");
 
   try {
-    const decoded = jwt.verify(token, config.get("jwtPrivateKey")); // decode the token and extract the payload
-    req.user = decoded; // set the payload in the response
+    const payload = jwt.verify(token, config.get("jwtPrivateKey")); // decode the token and extract the payload
+    req.user = payload; // set the payload in the response
     next(); // call the next middleware function in the pipeline
   } catch (error) {
     res.status(400).send("Invalid token.");
