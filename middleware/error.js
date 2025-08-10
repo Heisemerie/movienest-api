d7cda82b61a60ffeab0371c38f1f5d6864d6b40f
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-10 10:39:45 +0100

    fix: add stack trace to error middleware logs

--- middleware/error.js
+++ middleware/error.js
@@ -2,7 +2,7 @@ const winston = require("winston");
 
 function error(err, req, res, next) {
   // Log the exception
-  winston.error(err.message);
+  winston.error(err.message, err);
 
   res.status(500).send("Something failed.");
 }
