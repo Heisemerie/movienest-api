c5e2cf45824ed10c5a7538c489c18a91db1b4f61
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-10 14:54:25 +0100

    refactor: add levels to winston transports

--- startup/logging.js
+++ startup/logging.js
@@ -2,10 +2,17 @@ const winston = require("winston");
 require("winston-mongodb");
 
 module.exports = function (uri) {
-  // Add a file and console transport to the default logger
+  // Add a file, console and db transport to the default logger
   winston.add(new winston.transports.Console());
-  winston.add(new winston.transports.File({ filename: "combined.log" }));
-  winston.add(new winston.transports.MongoDB({ db: uri }));
+  winston.add(
+    new winston.transports.File({ filename: "combined.log", level: "info" })
+  );
+  winston.add(
+    new winston.transports.File({ filename: "error.log", level: "error" })
+  );
+  winston.add(new winston.transports.MongoDB({ db: uri, level: "error" }));
+
+  // Add uncaught exception and rejection handlers to default logger
   winston.exceptions.handle(
     new winston.transports.File({ filename: "uncaughtExceptions.log" }),
     new winston.transports.Console()
