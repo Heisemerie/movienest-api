2eb97e43dc001a2e087aa6e6d8b0b669a4baef23
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-10 14:18:56 +0100

    fix: add winston console transport to uncaught exceptions and unhandled rejections

--- startup/logging.js
+++ startup/logging.js
@@ -3,13 +3,15 @@ require("winston-mongodb");
 
 module.exports = function (uri) {
   // Add a file and console transport to the default logger
-  winston.add( new winston.transports.Console())
+  winston.add(new winston.transports.Console());
   winston.add(new winston.transports.File({ filename: "combined.log" }));
   winston.add(new winston.transports.MongoDB({ db: uri }));
   winston.exceptions.handle(
-    new winston.transports.File({ filename: "uncaughtExceptions.log" })
+    new winston.transports.File({ filename: "uncaughtExceptions.log" }),
+    new winston.transports.Console()
   );
   winston.rejections.handle(
-    new winston.transports.File({ filename: "unhandledRejections.log" })
+    new winston.transports.File({ filename: "unhandledRejections.log" }),
+    new winston.transports.Console()
   );
 };
