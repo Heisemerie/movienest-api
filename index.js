486b1bfb222e2868a27000878faa157656149d91
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-08-10 12:53:20 +0100

    refactor: extract routes

--- index.js
+++ index.js
@@ -2,17 +2,12 @@ require("dotenv").config(); // must come before `config` is used
 const winston = require("winston"); // comes with console transport
 require("winston-mongodb");
 const config = require("config");
-const express = require("express");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 const mongoose = require("mongoose");
-const error = require("./middleware/error");
-
+const express = require("express");
+const routes = require("./startup/routes");
 const app = express();
+
+routes(app);
 const uri = config.get("db.URI");
 
 // Add a file and console transport to the default logger
@@ -36,18 +31,6 @@ mongoose
   .connect(uri)
   .then(() => console.log("Connected to MongoDB replica set"))
   .catch((err) => console.error("MongoDB connection error:", err));
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error); // register after all middleware functions (called by async.js, handles express and mongodb request processing errors)
 
 // Express "endpoint" event emitter
 const port = config.get("port"); // get port from config
