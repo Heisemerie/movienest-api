3f0c9a9f660759b2fd19b5db39f6794fc20cce36
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-09-06 16:56:23 +0100

    set up the test db

--- startup/db.js
+++ startup/db.js
@@ -3,7 +3,5 @@ const winston = require("winston");
 
 module.exports = function (uri) {
   // Connect to MongoDB replica set
-  mongoose
-    .connect(uri)
-    .then(() => winston.info("Connected to MongoDB replica set"));
+  mongoose.connect(uri).then(() => winston.info(`Connected to ${uri}...`));
 };
