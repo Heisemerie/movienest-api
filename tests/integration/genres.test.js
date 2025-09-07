b810e0539fea4493bac23da1e84959e9213c311e
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-09-07 03:36:49 +0100

    write integration test for GET /api/genres/:id

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,5 +1,6 @@
 const request = require("supertest");
 const { Genre } = require("../../models/genre");
+const mongoose = require("mongoose");
 
 let server;
 
@@ -27,6 +28,25 @@ describe("/api/genres", () => {
       expect(res.body.length).toBe(2);
       expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
       expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
+    });
+  });
+
+  describe("GET /:id", () => {
+    it("should return genre if valid id is passed", async () => {
+      const genre = new Genre({ name: "genre1" });
+      await genre.save();
+
+      const res = await request(server).get(`/api/genres/${genre._id}`);
+
+      expect(res.status).toBe(200); // too generic
+      expect(res.body).toMatchObject({ name: "genre1" });
+      expect(res.body).toHaveProperty("name", "genre1");
+    });
+
+    it("should return 404 if invalid id is passed", async () => {
+      const res = await request(server).get("/api/genres/1");
+
+      expect(res.status).toBe(404);
     });
   });
 });
