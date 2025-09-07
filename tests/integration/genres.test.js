4ca410587636548fce27778748b2e5a3f2b104dd
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-09-07 02:56:53 +0100

    write integration test for GET /api/genres

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -1,4 +1,5 @@
 const request = require("supertest");
+const { Genre } = require("../../models/genre");
 
 let server;
 
@@ -7,14 +8,25 @@ describe("/api/genres", () => {
   beforeEach(() => {
     server = require("../../index");
   });
-  afterEach(() => {
+  afterEach(async () => {
     server.close();
+    await Genre.deleteMany({}); // cleanup after modifying the state to make test repeatable
   });
 
   describe("GET", () => {
     it("should return all genres", async () => {
+      // Populate the Test DB
+      await Genre.collection.insertMany([
+        { name: "genre1" },
+        { name: "genre2" },
+      ]);
+
       const res = await request(server).get("/api/genres");
+
       expect(res.status).toBe(200); // too generic
+      expect(res.body.length).toBe(2);
+      expect(res.body.some((g) => g.name === "genre1")).toBeTruthy();
+      expect(res.body.some((g) => g.name === "genre2")).toBeTruthy();
     });
   });
 });
