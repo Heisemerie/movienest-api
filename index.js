2162d6ece4a79f1226b5c013c3b3505a9b720089
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-10-01 22:58:00 +0100

    feat: integrate serverless-http for Vercel deployment and update package.json with new dependency

--- index.js
+++ index.js
@@ -2,22 +2,29 @@ require("dotenv").config(); // must come before `config` is used
 const config = require("config");
 const winston = require("winston");
 const express = require("express");
+const serverless = require("serverless-http");
+
 const app = express();
 
+// Load configs
 const uri = config.get("db.URI");
 const port = config.get("port");
 const jwtPrivateKey = config.get("jwtPrivateKey");
 
+// Startup modules
 require("./startup/logging")(uri);
 require("./startup/db")(uri);
 require("./startup/routes")(app);
 require("./startup/configs")(jwtPrivateKey);
 require("./startup/prod")(app);
 
-// Express "endpoint" event emitter
-const server = app.listen(port, () => {
-  winston.info(`Listening on port ${port}...`);
-});
+// Only start server in development
+if (!process.env.VERCEL) {
+  const server = app.listen(port, () => {
+    winston.info(`Listening on port ${port}...`);
+  });
 
-// Export server for Integration Test
-module.exports = server;
+  module.exports = server; // for integration tests, local dev
+} else {
+  module.exports = serverless(app); // for Vercel production
+}
