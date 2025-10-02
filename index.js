613ea9b21d2142504f7f5543b5f88dfeee3f493d
Mbah Chiemerie Ejike <ejike162@gmail.com>
2025-10-02 05:55:37 +0100

    feat: refactor server startup logic to improve development environment handling and remove node engine specification

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const app = express();
 
 // Load configs
 const uri = process.env.MONGODB_URI || config.get("db.URI");
-const port = config.get("port") || 3000;
 const jwtPrivateKey = process.env.JWT_SECRET || config.get("jwtPrivateKey");
 
 // Startup modules
@@ -17,8 +16,15 @@ require("./startup/routes")(app);
 require("./startup/configs")(jwtPrivateKey);
 require("./startup/prod")(app);
 
-const server = app.listen(port, () => {
-  winston.info(`Listening on port ${port}...`);
-});
+// Only start server in development
+if (!process.env.VERCEL) {
+  const port = config.get("port");
 
-module.exports = server; 
+  const server = app.listen(port, () => {
+    winston.info(`Listening on port ${port}...`);
+  });
+
+  module.exports = server; // for integration tests, local dev
+} else {
+  module.exports = app; // for Vercel production
+}
