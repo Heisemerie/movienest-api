bb96db2cfa96340b55d4e3cb247e48d4a18af51f
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-01-25 19:05:01 +0100

    refactor: add logs folder

--- startup/logging.js
+++ startup/logging.js
@@ -6,10 +6,13 @@ module.exports = function (uri) {
   // Add a file, console and db transport to the default logger
   winston.add(new winston.transports.Console());
   winston.add(
-    new winston.transports.File({ filename: "combined.log", level: "info" })
+    new winston.transports.File({
+      filename: "logs/combined.log",
+      level: "info",
+    }),
   );
   winston.add(
-    new winston.transports.File({ filename: "error.log", level: "error" })
+    new winston.transports.File({ filename: "logs/error.log", level: "error" }),
   );
   if (notVercel) {
     winston.add(new winston.transports.MongoDB({ db: uri, level: "error" }));
@@ -17,11 +20,11 @@ module.exports = function (uri) {
 
   // Add uncaught exception and rejection handlers to default logger
   winston.exceptions.handle(
-    new winston.transports.File({ filename: "uncaughtExceptions.log" }),
-    new winston.transports.Console()
+    new winston.transports.File({ filename: "logs/uncaughtExceptions.log" }),
+    new winston.transports.Console(),
   );
   winston.rejections.handle(
-    new winston.transports.File({ filename: "unhandledRejections.log" }),
-    new winston.transports.Console()
+    new winston.transports.File({ filename: "logs/unhandledRejections.log" }),
+    new winston.transports.Console(),
   );
 };
