24eda86248c0b81ce2b4bfa9d1967e510531c184
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-03-01 09:05:31 +0100

    fix: stop file logging in vercel

--- startup/logging.js
+++ startup/logging.js
@@ -5,26 +5,33 @@ const { notVercel } = require("./configs");
 module.exports = function (uri) {
   // Add a file, console and db transport to the default logger
   winston.add(new winston.transports.Console());
-  winston.add(
-    new winston.transports.File({
-      filename: "tmp/combined.log",
-      level: "info",
-    }),
-  );
-  winston.add(
-    new winston.transports.File({ filename: "tmp/error.log", level: "error" }),
-  );
-  if (notVercel) {
-    winston.add(new winston.transports.MongoDB({ db: uri, level: "error" }));
-  }
 
-  // Add uncaught exception and rejection handlers to default logger
-  winston.exceptions.handle(
-    new winston.transports.File({ filename: "tmp/uncaughtExceptions.log" }),
-    new winston.transports.Console(),
-  );
-  winston.rejections.handle(
-    new winston.transports.File({ filename: "tmp/unhandledRejections.log" }),
-    new winston.transports.Console(),
-  );
+  if (notVercel) {
+    winston.add(
+      new winston.transports.File({
+        filename: "logs/combined.log",
+        level: "info",
+      }),
+    );
+
+    winston.add(
+      new winston.transports.File({
+        filename: "logs/error.log",
+        level: "error",
+      }),
+    );
+
+    winston.add(new winston.transports.MongoDB({ db: uri, level: "error" }));
+
+    // Add uncaught exception and rejection handlers to default logger
+    winston.exceptions.handle(
+      new winston.transports.File({ filename: "logs/uncaughtExceptions.log" }),
+      new winston.transports.Console(),
+    );
+
+    winston.rejections.handle(
+      new winston.transports.File({ filename: "logs/unhandledRejections.log" }),
+      new winston.transports.Console(),
+    );
+  }
 };
